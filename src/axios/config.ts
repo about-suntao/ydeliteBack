e37019dd88668c00f0518bb27831b93e86816902
about-suntao <1550208137@qq.com>
2025-03-10 13:52:09 +0800

    build api配置修改

--- src/axios/config.ts
+++ src/axios/config.ts
@@ -6,7 +6,7 @@ const config: any = {
 if (process.env.NODE_ENV === 'development') {
     config.baseUrl = 'http://192.168.111.71:8081'
 } else if (process.env.NODE_ENV === 'production') {
-    config.baseUrl = 'http://applyback.ydelite.org'
+    config.baseUrl = 'https://applyback.ydelite.org'
 }
 
 export default config
